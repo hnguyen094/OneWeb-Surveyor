e730227f64256052f20cc3a349ed68dbab7af50d
Hung Nguyen <hnguyen@world-vu.net>
2017-07-07 21:37:26 -0700

    Got rotation vector working

---
+++ SurveyorNS/platforms/android/build/intermediates/assets/F0F1F2/debug/app/tns_modules/nativescript-rotation-vector/README.md
@@ -0,0 +1,37 @@
+# NativeScript Rotation Vector Plugin
+# Most of the code from NativeScript Accelerometer Plugin
+Rotation Vector plugin for NativeScript
+
+This plugin is compatible with both NativeScript `2.x` and `3.x` versions.
+
+Adapted from https://www.npmjs.com/package/nativescript-accelerometer
+Specifically, all the code are the same, with minor differences in android:
+* name changes from accelerometer to rotation Vector
+* matrix calculations in onSensorChanged
+* type of sensor queried (from TYPE_ACCELERROMETER to TYPE_ROTATION_VECTOR)
+
+## Usage
+```
+var rot = require("nativescript-rotation-vector");
+
+rot.startRotUpdates(function(data) {
+    console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
+}, { sensorDelay: "ui" });
+```
+
+## Expected Values
+
+ * x
+    * Spin Left/Right from -180 to 180 degrees
+ * y
+    * Tilt Up/Down from -90 to 90 degrees
+ * z
+    * Tilt Left/Right from -180 to 180 degrees
+
+## Options
+
+You can control how often the callback will be called by setting the `sensorDelay` option. The values are:
+* `"normal"` - Suitable for screen orientation changes. Around 0.2 seconds.
+* `"ui"` - Suitable for the user interface. Around 0.06 seconds.
+* `"game"` - Suitable for games. Around 0.02 seconds.
+* `"fastest"` - Sensor data as fast as possible.
--- SurveyorNS/platforms/android/src/main/assets/app/app.component.ts
+++ SurveyorNS/platforms/android/src/main/assets/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from "@angular/core";
 import * as camera from "nativescript-camera";
-import * as accelerometer from "nativescript-accelerometer";
+import * as rotVector from "nativescript-rotation-vector";
 import { Image } from "ui/image";
-import * as application from "application";
+import * as app from "application";
 
+// the @ represents a decorator that tells how this component/thing on the screen will look.
+// More here: https://github.com/Microsoft/TypeScript-Handbook/blob/master/pages/Decorators.md
 @Component({
   selector: "my-app",
-  template: `
-  <ActionBar title="{N} Camera Example"></ActionBar>
+  template: ` <!-- don't forget the ticks; it's important. They're for ES2015 template literals -->
+   <!--     Make sure they're <...></> and not just <.../>     -->
+  <ActionBar title="{N} Surveyor Camera"></ActionBar>
   <StackLayout horizontalAlignment="center" verticalAlignment="center">
-      <Image [src]="picture" width="200" height="200"></Image>
+      <Image [src]="picture" width="200" height="200"></Image> <!--http://nsimage.brosteins.com/-->
       <Button text="Capture" (tap)="takePicture()" class="btn btn-primary"></Button>
   </StackLayout>
   `
 })
+
 export class AppComponent {
   public picture: any;
   public roll: any;
   public pitch: any;
   constructor() {
     camera.requestPermissions();
-    accelerometer.startAccelerometerUpdates(function(data) {
-        this.roll = Math.atan2(data.y, data.z) * 180/Math.PI;
-        this.pitch = Math.atan2(-data.x, Math.sqrt(data.y*data.y + data.z*data.z)) * 180/Math.PI;
-        //console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
-        console.log("roll: " + this.roll + "pitch: " + this.pitch);
+
+    rotVector.startRotUpdates(function(data) {
+        console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
     }.bind(this));
     this.picture = "~/images/apple.jpg";
   }
