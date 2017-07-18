d1fbec49c8b7a4a0dd6471f332d3e36e8affa50a
Hung Nguyen <hnguyen@world-vu.net>
2017-07-18 12:33:14 -0700

    Commented on camerapreview.android

--- v2SurveyorNS/platforms/android/src/main/assets/app/nativescript-camera-preview/nativescript-camera-preview.js
+++ v2SurveyorNS/platforms/android/src/main/assets/app/nativescript-camera-preview/nativescript-camera-preview.js
@@ -1,11 +1,21 @@
 // TODO: Figure out if things commented with UNSURE are necessary
-
+ /*
+  * Function: implements the android version of the camera2 API for Android.
+  * Author: Hung Nguyen
+  * Many/all of the code comes from the two linked github repositories, with added features for Nativescript
+  * Works Cited:
+  * Android implementation: https://github.com/googlesamples/android-Camera2Basic/blob/master/Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
+  * Nativescript {N} implementation: https://github.com/NickIliev/NativeScript-cameraApp-poc
+  *
+  * Note: 'var' has been replaced with either 'let' or 'const' for the sake of scope. There shouldn't be any 'var's left.
+  * See here for more general info: https://www.typescriptlang.org/docs/handbook/variable-declarations.html
+  */
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let utilsModule = require("tns-core-modules/utils/utils");
-let platformModule = require("tns-core-modules/platform");
-let app = require('application');
-let common = require('./nativescript-camera-preview-common');
+const utilsModule = require("tns-core-modules/utils/utils");
+const platformModule = require("tns-core-modules/platform");
+const app = require('application');
+const common = require('./nativescript-camera-preview-common');
 
 const STATE_PREVIEW                 = 0; // UNSURE
 const STATE_WAITING_LOCK            = 1; // UNSURE
@@ -76,16 +86,15 @@ Function: captures a still picture. // UNSURE
 */
 const captureStillPicture = function() {
     // This is the CaptureRequest.Builder that we use to take a picture.
-    var captureBuilder = mCameraDevice.createCaptureRequest(android.hardware.camera2.CameraDevice.TEMPLATE_STILL_CAPTURE);
+    const captureBuilder = mCameraDevice.createCaptureRequest(android.hardware.camera2.CameraDevice.TEMPLATE_STILL_CAPTURE);
     captureBuilder.addTarget(mImageReader.getSurface());
     // Use the same AE and AF modes as the preview.
     captureBuilder.set(android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE, android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
             setAutoFlash(captureBuilder);
     // java class: extends the CaptureCallback class
-    var CaptureCallback = android.hardware.camera2.CameraCaptureSession.CaptureCallback.extend({
+    const CaptureCallback = android.hardware.camera2.CameraCaptureSession.CaptureCallback.extend({
         onCaptureCompleted: function (session, request, result) {
             console.log("onCaptureCompleted");
-            // console.log(mFile.toString());
         }
     });
     mCaptureSession.stopRepeating();
@@ -254,13 +263,14 @@ const CompareSizesByArea = java.lang.Object.extend({
   }
 });
 
-let AutoFitTextureView_constructorCalled = false; //TODO: See if I can add this inside?
-let mRatioWidth = 0;
-let mRatioHeight= 0;
 /**
 Class: a TextureView class. Extends the TextureView class.
 Used for autoresizing a TextureView by aspectRatio.
+Note: variables below are part of the class, but I am unsure if they could go inside the extend function
 */
+let AutoFitTextureView_constructorCalled = false; //TODO: See if I can add this inside?
+let mRatioWidth = 0;
+let mRatioHeight= 0;
 const AutoFitTextureView = android.view.TextureView.extend({
     /**
     Function: constructor
@@ -290,8 +300,8 @@ const AutoFitTextureView = android.view.TextureView.extend({
     */
     onMeasure: function(widthMeasureSpec, heightMeasureSpec) {
         this.super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        let width = this.super.getMeasuredWidth();
-        let height = this.super.getMeasuredHeight();
+        const width = this.super.getMeasuredWidth();
+        const height = this.super.getMeasuredHeight();
         console.log("width: " + width + " height: " + height + " ratioWidth: " + mRatioWidth + " ratioHeight: " + mRatioHeight);
         if (0 == mRatioWidth || 0 == mRatioHeight) {
           this.super.setMeasuredDimension(width, height);
@@ -305,47 +315,44 @@ const AutoFitTextureView = android.view.TextureView.extend({
     }
 });
 
-// from Java ; public static abstract class
-var MyCameraCaptureSessionStateCallback = android.hardware.camera2.CameraCaptureSession.StateCallback.extend({
+/**
+Class: from Java ; public static abstract class.
+See github repo at the link on the top of this page to better understand
+*/
+const MyCameraCaptureSessionStateCallback = android.hardware.camera2.CameraCaptureSession.StateCallback.extend({
     onConfigured: function(cameraCaptureSession) {
         console.log("onConfigured " + cameraCaptureSession);
-
         if (mCameraDevice === null) {
             return;
         }
-
         mCaptureSession = cameraCaptureSession;
-
-        // mPreviewRequestBuilder.set(android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE, android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-        // // Flash is automatically enabled when necessary.
-        // setAutoFlash(mPreviewRequestBuilder);
-
-        // Finally, we start displaying the camera preview.
-        mPreviewRequest = mPreviewRequestBuilder.build();
+        mPreviewRequest = mPreviewRequestBuilder.build(); // displaying the camera preview
         mCaptureSession.setRepeatingRequest(mPreviewRequest, new MyCaptureSessionCaptureCallback(), null);
-
     },
+
     onConfigureFailed: function(cameraCaptureSession) {
         console.log("onConfigureFailed " + cameraCaptureSession);
     }
 });
 
-// from Java : public static abstract class
-var MyCaptureSessionCaptureCallback = android.hardware.camera2.CameraCaptureSession.CaptureCallback.extend({
+/**
+Class: from Java ; public static abstract class.
+See github repo at the link on the top of this page to better understand
+*/
+const MyCaptureSessionCaptureCallback = android.hardware.camera2.CameraCaptureSession.CaptureCallback.extend({
     process: function(result) {
         switch (mState) {
-                case STATE_PREVIEW: {
-                    // We have nothing to do when the camera preview is working normally.
+                case STATE_PREVIEW: {// We have nothing to do when the camera preview is working normally.
                     break;
                 }
                 case STATE_WAITING_LOCK: {
-                    var afState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AF_STATE);
+                    const afState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AF_STATE);
                     if (afState === null) {
                         captureStillPicture();
                     } else if (android.hardware.camera2.CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED == afState ||
                             android.hardware.camera2.CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED == afState) {
                         // CONTROL_AE_STATE can be null on some devices
-                        var aeState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
+                        const aeState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
                         if (aeState === null ||
                                 aeState == android.hardware.camera2.CaptureResult.CONTROL_AE_STATE_CONVERGED) {
                             mState = STATE_PICTURE_TAKEN;
@@ -358,18 +365,18 @@ var MyCaptureSessionCaptureCallback = android.hardware.camera2.CameraCaptureSess
                 }
                 case STATE_WAITING_PRECAPTURE: {
                     // CONTROL_AE_STATE can be null on some devices
-                    var aeStatee = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
-                    if (aeStatee === null ||
-                            aeStatee == android.hardware.camera2.CaptureResult.CONTROL_AE_STATE_PRECAPTURE ||
-                            aeStatee == android.hardware.camera2.CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED) {
+                    const aeState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
+                    if (aeState === null ||
+                            aeState == android.hardware.camera2.CaptureResult.CONTROL_AE_STATE_PRECAPTURE ||
+                            aeState == android.hardware.camera2.CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED) {
                         mState = STATE_WAITING_NON_PRECAPTURE;
                     }
                     break;
                 }
                 case STATE_WAITING_NON_PRECAPTURE: {
                     // CONTROL_AE_STATE can be null on some devices
-                    var aeStateee = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
-                    if (aeStateee === null || aeStateee != android.hardware.camera2.CaptureResult.CONTROL_AE_STATE_PRECAPTURE) {
+                    const aeState = result.get(android.hardware.camera2.CaptureResult.CONTROL_AE_STATE);
+                    if (aeState === null || aeState != android.hardware.camera2.CaptureResult.CONTROL_AE_STATE_PRECAPTURE) {
                         mState = STATE_PICTURE_TAKEN;
                         captureStillPicture();
                     }
@@ -377,39 +384,42 @@ var MyCaptureSessionCaptureCallback = android.hardware.camera2.CameraCaptureSess
                 }
         }
     },
+
     onCaptureProgressed: function(session, request, partialResult) {
-        // console.log("onCaptureProgressed");
         this.process(partialResult);
     },
+
     onCaptureCompleted: function (session, request, result) {
-        // console.log("onCaptureCompleted");
         this.process(result);
     },
+
     onCaptureFailed: function (session, request, failure) {
-        // console.log("onCaptureFailed");
         console.log(failure);
     }
 });
 
-// (example for: java static interface to javaScript )
-// from Java : public static interface
-var mOnImageAvailableListener = new android.media.ImageReader.OnImageAvailableListener({
+/**
+(example for: java static interface to javaScript )
+from Java : public static interface
+See github repo at the link on the top of this page to better understand
+*/
+const mOnImageAvailableListener = new android.media.ImageReader.OnImageAvailableListener({
     onImageAvailable: function (reader) {
-
         // here we should save our image to file when image is available
         console.log("onImageAvailable");
         console.log(reader);
     }
 });
 
-// from Java : public static interface
-var mSurfaceTextureListener = new android.view.TextureView.SurfaceTextureListener({
-
+/**
+from Java : public static interface
+See github repo at the link on the top of this page to better understand
+*/
+const mSurfaceTextureListener = new android.view.TextureView.SurfaceTextureListener({
     onSurfaceTextureAvailable: function(texture, width, height) {
         console.log('onSurfaceTextureAvailable');
         mSurfaceTexture = texture;
         createCameraPreviewSession();
-        // openCamera(width, height);
     },
 
     onSurfaceTextureSizeChanged: function(texture) {
@@ -418,19 +428,19 @@ var mSurfaceTextureListener = new android.view.TextureView.SurfaceTextureListene
     },
 
     onSurfaceTextureDestroyed: function(texture) {
-        // console.log("onSurfaceTextureDestroyed");
         return true;
     },
 
     onSurfaceTextureUpdated: function(texture) {
       wrappedCallback();
-        // console.log("onSurfaceTexturUpdated");
     },
-
 });
 
-// from Java : public static abstract class
-var MyStateCallback = android.hardware.camera2.CameraDevice.StateCallback.extend({
+/**
+Class: from Java : public static abstract class
+See github repo at the link on the top of this page to better understand
+*/
+const MyStateCallback = android.hardware.camera2.CameraDevice.StateCallback.extend({
     onOpened: function(cameraDevice) {
         console.log("onOpened " + cameraDevice);
 
@@ -438,6 +448,7 @@ var MyStateCallback = android.hardware.camera2.CameraDevice.StateCallback.extend
         mCameraDevice = cameraDevice;
         createCameraPreviewSession();
     },
+
     onDisconnected: function(cameraDevice) {
         console.log("onDisconnected");
 
@@ -445,6 +456,7 @@ var MyStateCallback = android.hardware.camera2.CameraDevice.StateCallback.extend
         cameraDevice.close();
         mCameraDevice = null;
     },
+
     onError: function(cameraDevice, error) {
         console.log("onError");
         console.log("onError: device = " + cameraDevice);
@@ -454,6 +466,7 @@ var MyStateCallback = android.hardware.camera2.CameraDevice.StateCallback.extend
         cameraDevice.close();
         mCameraDevice = null;
     },
+
     onClosed: function(cameraDevice) {
         console.log("onClosed");
     }
