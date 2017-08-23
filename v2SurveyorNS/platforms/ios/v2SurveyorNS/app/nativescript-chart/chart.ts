8de19871fdf8d89338027b1596745afc481ba2c5
Hung Nguyen -mac <hnguyen@world-vu.net>
2017-08-23 01:08:24 -0700

    temp

--- v2SurveyorNS/platforms/ios/v2SurveyorNS/app/nativescript-chart/chart.ts
+++ v2SurveyorNS/platforms/ios/v2SurveyorNS/app/nativescript-chart/chart.ts
@@ -1,6 +1,7 @@
 import * as platform from "platform";
 import * as knownColors from "color/known-colors";
 import * as ImageModule from "tns-core-modules/ui/image";
+import * as layout from "ui/layouts/grid-layout";
 
 let page;
 const ele: number[] = [];
@@ -32,29 +33,32 @@ export function initGraph(myPage) {
   }
 }
 
-export function updateGraph(azimuth, elevation) {
+export function updateGraph(azimuth, elevation, isOn) {
   const az = Math.floor(azimuth) + 180;
-  ele[az] = -elevation<minEle? minEle: -elevation>maxEle? maxEle: -elevation;
+  layout.GridLayout.setColumn(page.getViewById("lcursor"), az);
+  if(isOn){
+    ele[az] = -elevation<minEle? minEle: -elevation>maxEle? maxEle: -elevation;
 
-  const currentView = page.getViewById("l"+az);
-  currentView.height = maxHeight * ele2Percent(ele[az]);
+    const currentView = page.getViewById("l"+az);
+    currentView.height = maxHeight * ele2Percent(ele[az]);
 
-  const dif = az-prevIndex;
-  if(Math.abs(dif) > 1 && Math.abs(dif) < smoothingRange) {
-    let start, end;
-    if(dif > 0) {
-      start = prevIndex + 1;
-      end = az;
-    } else {
-      start = az + 1;
-      end = prevIndex;
+    const dif = az-prevIndex;
+    if(Math.abs(dif) > 1 && Math.abs(dif) < smoothingRange) {
+      let start, end;
+      if(dif > 0) {
+        start = prevIndex + 1;
+        end = az;
+      } else {
+        start = az + 1;
+        end = prevIndex;
+      }
+      for (let i = start; i < end; i++) {
+        ele[i] = ele[start-1] + (i - start+1) / (dif-1) * (ele[az]-ele[prevIndex]);
+        page.getViewById("l"+(i)).height = maxHeight * ele2Percent(ele[i]);
+      }
     }
-    for (let i = start; i < end; i++) {
-      ele[i] = ele[start-1] + (i - start+1) / (dif-1) * (ele[az]-ele[prevIndex]);
-      page.getViewById("l"+(i)).height = maxHeight * ele2Percent(ele[i]);
-    }
+    prevIndex = az;
   }
-  prevIndex = az;
 }
 
 function ele2Percent(elevation) {
